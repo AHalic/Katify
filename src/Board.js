1d8ac59baff81f9080c49834f796d441df72ac24
AHalic <sophie.gama@aluno.ufes.br>
2021-09-05 19:03:52 -0300

    :sparkles: add: classes with stardand attributes and methods of kanban
    Co-authored-by: Bea Maia <beamaia@users.noreply.github.com>

---
+++ src/Board.js
@@ -0,0 +1,34 @@
+import Status from './Status';
+import Column from './Column';
+
+import { v4 as uuidv4 } from 'uuid';
+
+export default class Board {
+    constructor() {
+        this._boardName = "Undefined"
+        this._columns = [];
+        this.createCol(Status.done);
+        this.createCol(Status.toDo);
+        this.createCol(Status.inProgress);
+        this.createCol(Status.discarded);
+
+        // TODO create uuid
+        this._uuid = uuidv4();
+    }
+
+    createCol (status) {
+        this._columns.push(new Column(status));
+    }
+
+    get boardName() {
+        return this._boardName;
+    }
+
+    set boardName(name) {
+        this._boardName = name;
+    }
+
+    get uuid() {
+        return this._uuid;
+    }
+}
---
+++ src/Column.js
@@ -0,0 +1,17 @@
+import CatCard from "./CatCard";
+
+export default class Column {
+    constructor(status) {
+        this._name = status;
+        this._cards = [];
+    }   
+
+    pushCard(card) {
+        this._cards.push(card);
+    }
+
+    removeCard(card) {
+        if (this._cards.includes(card))
+            this._cards.splice(this._cards.indexOf(card), 1);
+    }
+}
