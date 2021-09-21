230fd2e72015ff4fc876a40e9f48f499a10de2a4
bea <beamaia.work@gmail.com>
2021-09-21 18:24:58 -0300

    :recycle: refactor: adds board
    Co-authored-by: AHalic <AHalic@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // document.body.style.background = "#2D3035";
 // document.body.getElementsByClassName("card")
+import Board from './src/Board'
+
+board = new Board();
+
 
 
 const express = require('express')
@@ -9,3 +13,4 @@ const app = express()
 app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
 
 app.get('/', (req, res) => (res.send("Servidor rodando, test 123")))
+
