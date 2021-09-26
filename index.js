80db6f9399adf0bb353494380a2a03f0b44cb079
bea <beamaia.work@gmail.com>
2021-09-26 14:06:55 -0300

    :sparkles: feat: shows index html on localhost

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-// const express = require('express')
+const express = require('express')
 
-// const app = express()
+const app = express()
+app.use(express.static(__dirname));
 
-// app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
+app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
 
-// app.get('/', (req, res) => (res.send("Servidor rodando, test 123")))
+app.get("/", (req, res) => {
+
+    res.sendFile(__dirname+"/index.html");
+});
 
 
+// import {Board} from './src/Board.js';
+// import {addDiscardHandler, addDoneHandler, addProgressHandler, addTodoHandler} from './src/Events.js'
 
-import Board from '/src/Board.js';
-import {addDiscardHandler, addDoneHandler, addProgressHandler, addTodoHandler} from '/src/Events.js'
+// let board = new Board();
+// board.boardName = "Kateste"
 
-let board = new Board();
-board.boardName = "Kateste"
+// console.log(board);
 
-console.log(board);
+// addDiscardHandler(document.getElementsByClassName("discard-button")[0], board);
 
-addDiscardHandler(document.getElementsByClassName("discard-button")[0], board);
+// addDoneHandler(document.getElementsByClassName("done-button")[0], board);
 
-addDoneHandler(document.getElementsByClassName("done-button")[0], board);
+// addTodoHandler(document.getElementsByClassName("todo-button")[0], board);
 
-addTodoHandler(document.getElementsByClassName("todo-button")[0], board);
-
-addProgressHandler(document.getElementsByClassName("inProgress-button")[0], board);
+// addProgressHandler(document.getElementsByClassName("inProgress-button")[0], board);
