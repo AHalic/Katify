ca718161dd15a4ea8de6546b9407d239812cf735
bea <beamaia.work@gmail.com>
2021-09-26 18:27:03 -0300

    :sparkles: add: adds custom express
    Co-authored-by: AHalic <AHalic@users.noreply.github.com>

---
+++ src/config/customExpress.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const consign = require('consign');
+const bodyParser = require('body-parser');
+ 
+module.exports = () => {
+    const app = express();
+    
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true}))
+    consign()
+        .include('controllers')
+        .into(app)
+    
+    return app;
+}
