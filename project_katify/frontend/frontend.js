f597e055adcc49e77bed05a41da607c1b54828de
AHalic <sophie.gama@aluno.ufes.br>
2021-09-27 20:25:39 -0300

    :recycle: refactor: changes import and export and comments place
    Co-authored-by: Bea Maia <beamaia@users.noreply.github.com>

--- project_katify/frontend/frontend.js
+++ project_katify/frontend/frontend.js
@@ -1,5 +1,5 @@
-// import { Events } from './Events.js';
-//import board, { boardName } from './Board.js';
+import Events from './Events.js';
+import Board from './Board.js';
 
 const url = "http://localhost:3000"
 
@@ -7,26 +7,19 @@ fetch(url)
   .then(function(response) {
     response.json().then(function(data) {
       console.log(data)
+      // boardName = "Kateste"
+      
+      // Events.addDiscardHandler(document.getElementsByClassName("discard-button")[0], board);
+      
+      // Events.addDoneHandler(document.getElementsByClassName("done-button")[0], board);
+      
+      // Events.addTodoHandler(document.getElementsByClassName("todo-button")[0], board);
+      
+      // Events.addProgressHandler(document.getElementsByClassName("inProgress-button")[0], board);
     })
   })
   .catch(function(error){
     console.log("deu ruim")
   })
 
-console.log("oi estou aqui frontend");
 
-//import api from 'api_axios.js'
-
-console.log("cheguei");
-
-
-
-// boardName = "Kateste"
-
-// Events.addDiscardHandler(document.getElementsByClassName("discard-button")[0], board);
-
-// Events.addDoneHandler(document.getElementsByClassName("done-button")[0], board);
-
-// Events.addTodoHandler(document.getElementsByClassName("todo-button")[0], board);
-
-// Events.addProgressHandler(document.getElementsByClassName("inProgress-button")[0], board);
