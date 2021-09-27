3eef83739de3809054cae63472b615d7c6f13b97
AHalic <sophie.gama@aluno.ufes.br>
2021-09-26 22:37:53 -0300

    :sparkles: add: add http requests routes
    Co-authored-by: Bea Maia <beamaia@users.noreply.github.com>

--- src/controllers/card.js
+++ src/controllers/card.js
@@ -1,13 +1,38 @@
 const Card = require('../models/card')
 
 module.exports = app => {
-    app.get('/', (req, res) => res.send('Você está na rota root e está realizando um GET'));
-
-
     app.post('/', (req, res) => {
-       const card = req.body;
+        const card = req.body;
 
         Card.adiciona(card, res)
     })
     
+    app.get('/', (req, res) => {
+        Card.lista(res)
+    })
+
+    app.get('/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+
+        Card.buscaPorId(id, res)
+    })
+
+    app.post('/', (req, res) => {
+       const Card = req.body
+
+        Card.adiciona(Card, res)
+    }) 
+
+    app.patch('/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+
+        Card.altera(id, valores, res)
+    })
+
+    app.delete('/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+
+        Card.deleta(id, res)
+    })
 }
