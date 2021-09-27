03874af8b5a1e9baaf24a670d4e53a0fc1ad1df8
AHalic <sophie.gama@aluno.ufes.br>
2021-09-27 17:46:11 -0300

    :recycle: fix:  solve CORS problem
    Co-authored-by: Bea Maia <beamaia@users.noreply.github.com>

---
+++ project_katify/backend/config/customExpress.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const consign = require('consign');
+ 
+module.exports = () => {
+    const app = express();
+    
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true}))
+    app.use(function(req, res, next) {
+        // adiciona header the cors
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); 
+        
+        // garante que os headers sao aplicado
+        next();
+    });
+    consign()
+        .include('backend/controllers')
+        .into(app)
+
+    return app;
+}
