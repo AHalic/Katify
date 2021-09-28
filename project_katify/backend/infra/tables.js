0222c18c6edaf16ab7a175acded5ac388c198ff1
AHalic <sophie.gama@aluno.ufes.br>
2021-09-28 20:47:52 -0300

    :recycle: feat: adds column to db
    Co-authored-by: Bea Maia <beamaia@users.noreply.github.com>

--- project_katify/backend/infra/tables.js
+++ project_katify/backend/infra/tables.js
@@ -3,16 +3,30 @@ class Tables {
         this.conexao = conexao
 
         this.createCardTable()
+
+        this.createBoardTable()
     }
 
     createCardTable() {
-        const sql = 'CREATE TABLE IF NOT EXISTS KatifyBoard (id int NOT NULL AUTO_INCREMENT, name varchar(45) NOT NULL, tag1 varchar(15), tag2 varchar(15), description text, status varchar(15) NOT NULL, PRIMARY KEY(id))'
+        const sql = 'CREATE TABLE IF NOT EXISTS Cards (id int NOT NULL AUTO_INCREMENT, uuid varchar(40) NOT NULL, name varchar(45) NOT NULL, tag1 varchar(15), tag2 varchar(15), description text, status varchar(15) NOT NULL, PRIMARY KEY(id))'
 
         this.conexao.query(sql, erro => {
             if(erro) {
                 console.log(erro)
             } else {
-                console.log('Tabela KatifyBoard criada com sucesso')
+                console.log('Tabela Cards criada com sucesso')
+            }
+        })
+    }
+
+    createBoardTable() {
+        const sql = 'CREATE TABLE IF NOT EXISTS Boards (id int NOT NULL AUTO_INCREMENT, uuid varchar(40) NOT NULL, name varchar(45) NOT NULL, PRIMARY KEY(id))';
+
+        this.conexao.query(sql, erro => {
+            if(erro) {
+                console.log(erro)
+            } else {
+                console.log('Tabela Boards criada com sucesso')
             }
         })
     }
