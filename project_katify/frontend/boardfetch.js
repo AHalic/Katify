9cead3bf9ce950774f60f028d74ec08a7188d348
AHalic <sophie.gama@aluno.ufes.br>
2021-09-29 20:06:41 -0300

    :recycle: refactor: changes server fetch
    Co-authored-by: Bea Maia <beamaia@users.noreply.github.com>

--- project_katify/frontend/boardfetch.js
+++ project_katify/frontend/boardfetch.js
@@ -1,28 +1,25 @@
+import api from './api.js'
+
 const url = "http://localhost:3000"
 const boarduuid = document.URL.split('/')[3]
 
-fetch(url)
-	.then(function(response) {
-		response.json().then(function(data) {
-			let name;
-            for (let board of data) {
-				if (board.uuid === boarduuid) {
-					document.getElementsByClassName("workspace-name")[0].value = board.name  //.innerHTML = board.name 
-				}
-			}
-		})
-	})
-	.catch(function(error){
-		console.log("Something went wrong: can't connect to backend")
-	})
+let workspaceInput = document.getElementsByClassName("workspace-name")[0]
+let workspaceForm = document.getElementById("workspace-name-form")
 
+api.get('').then(res => {
+	let name;
 
-fetch(url + "/" + boarduuid)
-	.then(function(response) {
-		response.json().then(function(data) {
-			console.log(data)
-		})
-	})
-	.catch(function(error){
-		console.log("Something went wrong: can't connect to backend")
-	})
+	for (let board of res.data) {
+		if (board.uuid === boarduuid) {
+			document.getElementsByClassName("workspace-name")[0].value = board.name
+			document.title = board.name
+		}
+	}
+
+})
+
+workspaceForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+	api.patch(`/${boarduuid}`, { name: `${workspaceInput.value}` })
+	return false;
+})
