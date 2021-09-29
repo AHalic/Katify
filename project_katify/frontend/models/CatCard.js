dd02d0d4dee3160130421fb8f520e34cb85d5f6f
AHalic <sophie.gama@aluno.ufes.br>
2021-09-29 20:00:04 -0300

    :recycle: change: changes Card constructor and methods
    Co-authored-by: Bea Maia <beamaia@users.noreply.github.com>

--- project_katify/frontend/models/CatCard.js
+++ project_katify/frontend/models/CatCard.js
@@ -1,50 +1,68 @@
-import Status from './Status.js';
+import Status from './Status.js'
 
 class CatCard {
-    constructor() {
-        this._name = "Untitled";
-        this._status =  Status.without;
-        this._description = "";
-        this._tags = [];
+    constructor(name, status, description, tags, id, uuid) {
+        this._name = name
+        this._status =  status
+        this._description = description
+        this._tags = tags
+        this.__id = id
+        this.__uuid = uuid
     }
 
     set name(name) {
-        this._name = name;
+        this._name = name
     }
 
     get name() {
-        return this._name;
+        return this._name
     }
 
     pushTag(tag) {
         if (!this._tags.includes(tag))
-            this._tags.push(tag);
+            this._tags.push(tag)
     }
 
     get tags() {
-        return this._tags;
+        return this._tags
     }
 
     removeTag(tag) {
         if (this._tags.includes(tag))
-            this._tags.splice(this._tags.indexOf(tag), 1);
+            this._tags.splice(this._tags.indexOf(tag), 1)
     } 
 
     set description(description) {
-        this._description = description;
+        this._description = description
     }
 
     get description() {
-        return this._description;
+        return this._description
     }
 
     set status(status) {
-        this._status = status;
+        this._status = status
     }
 
     get status() {
-        return this._status;
+        return this._status
+    }
+
+    get id() {
+        return this.__id
+    }
+
+    set id(id) {
+        this.__id = id
+    }
+
+    get uuid() {
+        return this.__uuid
+    }
+
+    set uuid(uuid) {
+        this.__uuid = uuid
     }
 }
 
-export default CatCard;
+export default CatCard
