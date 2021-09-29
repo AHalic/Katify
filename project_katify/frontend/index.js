3200feab519776708e62ff682cf73bcf14e2fb28
AHalic <sophie.gama@aluno.ufes.br>
2021-09-29 11:14:58 -0300

    :sparkles: add: front server
    Co-authored-by: Bea Maia <beamaia@users.noreply.github.com>

---
+++ project_katify/frontend/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static(__dirname))
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/homePage.html'))
+})
+
+app.get('/:uuid', function(req, res) {
+    res.sendFile(path.join(__dirname, '/boardPage.html'))
+})
+
+app.listen(8000, () => console.log('Frontend 8000'))
