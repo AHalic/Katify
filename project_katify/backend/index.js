01548938f7e650733135b8ca6eb272ba0f96d840
AHalic <sophie.gama@aluno.ufes.br>
2021-09-29 11:11:51 -0300

    :hammer: change: changes file repository
    Co-authored-by: Bea Maia <beamaia@users.noreply.github.com>

---
+++ project_katify/backend/index.js
@@ -0,0 +1,21 @@
+const customExpress = require('./config/customExpress')
+const conexao = require('./infra/conection')
+const Tabelas = require('./infra/tables')
+const express = require('express');
+const cors = require('cors')
+
+conexao.connect(erro => {
+    if(erro) {
+        console.log(erro);
+    } else {
+        console.log('conectado com sucesso');
+        
+        Tabelas.init(conexao);
+        
+        const app = customExpress();
+
+        app.use('/', express.static(__dirname + '/api/'));
+        app.use(cors())
+        app.listen(3000, () => console.log('Servidor Backend, rodando na porta 3000'));
+    }
+})
---
+++ project_katify/frontend/models/Tag.js
@@ -0,0 +1,27 @@
+export class Tag {
+    constructor(name="Untitled") {
+        this.__name = name;
+        this.__color = Math.round(Math.random() * (4 - 1) +1);
+    }    
+}
+
+export class Tags {
+    constructor() {
+        this.__list = [];
+    }
+
+    get list(){
+        return this.__list;
+    }
+    
+  
+    tagInList(name) {
+        for (let i of this.__list) {
+            if (i.__name == name)
+                return true;
+        }
+
+        return false;
+    }
+    
+}
