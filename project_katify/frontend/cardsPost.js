db0d406438dffb2561905c17c61b617555f4edbd
AHalic <sophie.gama@aluno.ufes.br>
2021-09-30 02:41:18 -0300

    :sparkles: feat: adds card modal
    Co-authored-by: Bea Maia <beamaia@users.noreply.github.com>

--- project_katify/frontend/cardsPost.js
+++ project_katify/frontend/cardsPost.js
@@ -1,4 +1,27 @@
 import api from './api.js'
+import Status from './models/Status.js'
+
+const boarduuid = document.URL.split('/')[3]
+const url = `http://localhost:3000`
+
+function postCardDiscarded() {
+    let status = Status.discarded
+
+
+    
+    let inProgColBox = document.getElementsByClassName("box-discarded")[0]
+    let outerDiv = document.createElement("div")
+    outerDiv.className = "card"
+
+    let innerDiv = document.createElement("div")
+    innerDiv.className = "card-body"
+
+    outerDiv.appendChild(innerDiv)    
+    // let t = document.createTextNode("This is a paragraph.")
+    // innerDiv.appendChild(t)
+    inProgColBox.appendChild(outerDiv)   
+
+}
 
 function postCardInProgress() {
     let inProgColBox = document.getElementsByClassName("box-inProgress")[0]
@@ -12,7 +35,7 @@ function postCardInProgress() {
     // let t = document.createTextNode("This is a paragraph.")
     // innerDiv.appendChild(t)
     outerDiv.appendChild(innerDiv)    
-    inProgColBox.appendChild(outerDiv)    
+    inProgColBox.appendChild(outerDiv)
 }
 
 function postCardToDo() {
@@ -45,19 +68,9 @@ function postCardDone() {
     inProgColBox.appendChild(outerDiv)    
 }
 
-function postCardDiscarded() {
-    let inProgColBox = document.getElementsByClassName("box-discarded")[0]
-
-    let outerDiv = document.createElement("div")
-    outerDiv.className = "card"
-
-    let innerDiv = document.createElement("div")
-    innerDiv.className = "card-body"
-
-    // let t = document.createTextNode("This is a paragraph.")
-    // innerDiv.appendChild(t)
-    outerDiv.appendChild(innerDiv)    
-    inProgColBox.appendChild(outerDiv)    
+function clickedPostCardDiscarded() {
+    let createTask = document.getElementsByClassName("create-task-btn")[0]
+    createTask.addEventListener('click', postCardDiscarded, false) 
 }
 
 let inProgButEvent = document.getElementsByClassName("x-button")[0]
@@ -68,4 +81,4 @@ let discardedButEvent = document.getElementsByClassName("x-button")[3]
 inProgButEvent.addEventListener('click', postCardInProgress, true);
 toDoButEvent.addEventListener('click', postCardToDo, true);
 doneButEvent.addEventListener('click', postCardDone, true);
-discardedButEvent.addEventListener('click', postCardDiscarded, true);
+discardedButEvent.addEventListener('click', clickedPostCardDiscarded, true);
