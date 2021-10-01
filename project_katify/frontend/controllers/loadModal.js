5064009a6a342786bfa45542fa25e03091aee470
AHalic <sophie.gama@aluno.ufes.br>
2021-10-01 15:10:57 -0300

    :sparkles: feat: adds patch feature
    Co-authored-by: Bea Maia <beamaia@users.noreply.github.com>

--- project_katify/frontend/controllers/loadModal.js
+++ project_katify/frontend/controllers/loadModal.js
@@ -1,29 +1,89 @@
 import api from '../api.js'
 import deleteCard from './cardsDelete.js'
+import Status from '../models/Status.js'
+import CatCard from '../models/CatCard.js'
+import randomColor from './randomColor.js'
 
 const boarduuid = document.URL.split('/')[3]
 
-function dataValue(value, compValue, atr) {
-    let data = {}
+function addCard(card, id) {
+    let colBox 
 
-    if (value.normalize() === compValue.normalize()) {
-        data = {
-            atr: compValue
-        }
+    if (card.status == Status.done){
+        colBox = document.getElementsByClassName("box-done")[0]
+    }
+    else if (card.status == Status.toDo){
+        colBox = document.getElementsByClassName("box-toDo")[0]
+    }
+    else if (card.status == Status.discarded){
+        colBox = document.getElementsByClassName("box-discarded")[0]  
+    }
+    else if (card.status == Status.inProgress){
+        colBox = document.getElementsByClassName("box-inProgress")[0]
     }
 
-    return data
+    let cardClass = new CatCard(card.name, card.status, card.description, [card.tag1, card.tag2], id, boarduuid)
+
+    let outerDiv = document.createElement("div")
+    outerDiv.className = "card"
+    outerDiv.setAttribute("id", id)
+    outerDiv.setAttribute("data-toggle", "modal")
+    outerDiv.setAttribute("data-target", "#editCardModal")
+    outerDiv.setAttribute("data-watherver", "@fat")
+
+    let innerDiv = document.createElement("div")
+    innerDiv.className = "card-body"
+
+    let cardName = document.createTextNode(card.name)
+
+    innerDiv.appendChild(cardName)
+    outerDiv.appendChild(innerDiv)
+    
+    let tagRow = document.createElement("div")
+    tagRow.classList.add("row")
+
+    if (card.tag1.length != 0) {
+        let tagOne = document.createTextNode(card.tag1)
+        let tagOneDiv = document.createElement("div")
+        tagOneDiv.classList.add("tag-color", randomColor())
+
+        tagOneDiv.appendChild(tagOne)
+        tagRow.appendChild(tagOneDiv)
+        tagRow.classList.add("tag-row")
+
+    } 
+
+    if (card.tag2.length != 0) {
+        let tagTwo = document.createTextNode(card.tag2)
+        let tagTwoDiv = document.createElement("div")
+        tagTwoDiv.classList.add("tag-color", randomColor())
+
+        tagTwoDiv.appendChild(tagTwo)
+        tagRow.appendChild(tagTwoDiv)
+        tagRow.classList.add("tag-row")
+    }
+
+    outerDiv.appendChild(tagRow)
+    colBox.appendChild(outerDiv)  
+
+    document.getElementById("modalForm").reset()
+    loadEditModal(outerDiv, cardClass)
 }
 
-function sendCard(data, id) {
-    // let name = data.name
-    // let tag1 = data.tags[1]
-    // let tag2 = data.tags[2]
-    // let description = 
-    console.log("inside send card", id)
-    for (let i = 0; i < 4; i++) {
-        // console.log(data[i].value)
+function sendCard(data, id, card) {
+    const editedCard = {
+        name: data[0].value,
+        tag1: data[1].value,
+        tag2: data[2].value,
+        status: data[3].value,
+        description: data[4].value
     }
+
+    api.patch(`/${boarduuid}/${id}`, editedCard)
+    document.getElementById(id).remove();
+    addCard(editedCard, id)
+
+    return false
 }
 
 function loadEditModal(outerDiv, card) {
@@ -53,6 +113,7 @@ function loadEditModal(outerDiv, card) {
         // recoloca todas as opcoes, colocando como selected a do status
         for (let i = 0; i < 4; i++) {
             let opt = document.createElement('option')
+            opt.className = "status-option"
             opt.value = statusList[i]
             opt.innerHTML = statusList[i]
             
@@ -63,25 +124,19 @@ function loadEditModal(outerDiv, card) {
             select.appendChild(opt)
         }
 
-        let data = document.forms["modalForm"].elements
-        console.log(data)
-        console.log(id)
-        for (let i = 0; i < 4; i++) {
-            console.log(data[i].value)
-        }
-    
+        let data = document.forms["editModalForm"].elements
         let editButton = document.getElementsByClassName("edit-task-btn")[0]
-        console.log(editButton)
-        editButton.addEventListener("click", function() {
-            console.log(`data no evento ${data}`)
-            sendCard(data, id)})
+
+        editButton.onclick = function() {
+            sendCard(data, id, card)
+        }
 
         let deleteBtn = document.getElementsByClassName("btn-delete")[0]
         deleteBtn.addEventListener("click", function() {
-            deleteCard(id)})
+            deleteCard(id)
+        })
 
     })
-    
 }
 
 export default loadEditModal;
