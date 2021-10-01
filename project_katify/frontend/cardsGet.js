b4fa008e5e1bbcf84c314698b09591d5fd3c30f6
bea <beamaia.work@gmail.com>
2021-10-01 00:06:10 -0300

    :recycle: refactor: gets cards
    Co-authored-by: AHalic <AHalic@users.noreply.github.com>

--- project_katify/frontend/cardsGet.js
+++ project_katify/frontend/cardsGet.js
@@ -6,19 +6,18 @@ const boarduuid = document.URL.split('/')[3]
 const url = `http://localhost:3000`
 
 function getStatus(status) {
-    if (status == "Done")
+    if (String(status).valueOf() == new String("Done").valueOf())
         return Status.done
-    else if (status == "In Progress") 
+    else if (String(status).valueOf() == new String("In Progress").valueOf()) 
         return Status.inProgress
-    else if (status == "To-Do")
+    else if (String(status).valueOf() == new String("To Do").valueOf())
         return Status.toDo
-    else if (status == "Discarded")
+    else if (String(status).valueOf() == new String("Discarded").valueOf())
         return Status.discarded
 }
 
 function createCard(data) {
     let card = new CatCard(data.name, getStatus(data.status), data.description, [data.tag1, data.tag2], data.id, data.uuid) 
-
     let colBox 
     
     if (card.status == Status.done)
@@ -30,7 +29,6 @@ function createCard(data) {
     else if (card.status == Status.inProgress)
         colBox = document.getElementsByClassName("box-inProgress")[0]
 
-    
     let outerDiv = document.createElement("div")
     outerDiv.className = "card"
     outerDiv.setAttribute("id", card.id);
@@ -40,22 +38,41 @@ function createCard(data) {
 
     let cardName = document.createTextNode(card.name)
     
-
-
     innerDiv.appendChild(cardName)
-    outerDiv.appendChild(innerDiv)    
+    outerDiv.appendChild(innerDiv)  
+    
+    let tagRow = document.createElement("div")
+    tagRow.classList.add("row")
+
+    console.log(card.tags)
+
+    if (card.tags[0].length != 0) {
+        let tagOne = document.createTextNode(card.tags[0])
+        let tagOneDiv = document.createElement("div")
+        tagOneDiv.classList.add("tag-pink")
+
+        tagOneDiv.appendChild(tagOne)
+        tagRow.appendChild(tagOneDiv)
+        tagRow.classList.add("tag-row")
+
+    } 
+
+    if (card.tags[1].length != 0) {
+        let tagTwo = document.createTextNode(card.tags[1])
+        let tagTwoDiv = document.createElement("div")
+        tagTwoDiv.classList.add("tag-pink")
+
+        tagTwoDiv.appendChild(tagTwo)
+        tagRow.appendChild(tagTwoDiv)
+        tagRow.classList.add("tag-row")
+    }
+
+    outerDiv.appendChild(tagRow)  
     colBox.appendChild(outerDiv)      
 }
 
 api.get(`/${boarduuid}`).then(res => {
     for (let data of res.data) {
-        console.log(data)
         createCard(data)
     }
 })
-
-// workspaceForm.addEventListener("submit", (event) => {
-// 	event.preventDefault();
-// 	api.patch(`/${boarduuid}`, { name: `${workspaceInput.value}` })
-// 	return false;
-// })
