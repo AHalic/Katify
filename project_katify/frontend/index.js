4dc065c7a089c8141f8eaeca687586126751a807
AHalic <sophie.gama@aluno.ufes.br>
2021-10-08 11:36:01 -0300

    :bulb: docs: adds file documentation

--- project_katify/frontend/index.js
+++ project_katify/frontend/index.js
@@ -1,13 +1,23 @@
+/*
+ * Descricao: Configuracao do servidor do frontend
+ * Data: Outubro 2021
+ * @version 1.0
+ * @author Beatriz Maia & Sophie Dilhon
+*/
+
 const express = require('express')
 const path = require('path')
 const app = express()
 
 app.use(express.static(__dirname))
 
+
+// Pega todos os boards da tabela e direciona para a página home
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/homePage.html'))
 })
 
+// Pega todos os cards do board e direciona para a página do board
 app.get('/:uuid', function(req, res) {
     res.sendFile(path.join(__dirname, '/boardPage.html'))
 })
