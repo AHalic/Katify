b93777b0840e50fb228bcf713b748f1552ab26b4
AHalic <sophie.gama@aluno.ufes.br>
2021-10-08 11:35:55 -0300

    :bulb: docs: adds file documentation

--- project_katify/frontend/api.js
+++ project_katify/frontend/api.js
@@ -1,3 +1,10 @@
+/*
+ * Descricao: cria api para conexão do back com o front
+ * Data: Outubro 2021
+ * @version: 1.0
+ * @author Beatriz Maia & Sophie Dilhon
+ */ 
+
 const api = axios.create({
   baseURL: 'http://localhost:3000'
 })
