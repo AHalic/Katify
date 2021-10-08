6871f41e13a14f47dc61973d1cf68b232813538a
AHalic <sophie.gama@aluno.ufes.br>
2021-10-08 11:10:14 -0300

    :bulb: docs: adds file documentation

--- project_katify/backend/index.js
+++ project_katify/backend/index.js
@@ -1,9 +1,19 @@
+/*
+ * Descricao: Configuracao do servidor do backend
+ * Data: Outubro 2021
+ * @version 1.0
+ * @author Beatriz Maia & Sophie Dilhon
+*/
+
 const customExpress = require('./config/customExpress')
 const connection = require('./infra/connection')
 const Tabelas = require('./infra/tables')
 const express = require('express');
 const cors = require('cors')
 
+/**
+ * Conecta com o servidor backend e cria as tabelas do bd
+ */
 connection.connect(erro => {
     if(erro) {
         console.log(erro);
