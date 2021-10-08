84cc2d8115bc98d3f98ca39cbda4ff783c890275
bea <beamaia.work@gmail.com>
2021-10-08 11:45:50 -0300

    :bulb: docs: adds comments related to class

--- project_katify/frontend/models/CatCard.js
+++ project_katify/frontend/models/CatCard.js
@@ -1,4 +1,23 @@
+/*
+ * Descricao: Arquivo contendo a classe referente as cards
+ * Data: Outubro 2021
+ * @version 1.0
+ * @author Beatriz Maia & Sophie Dilhon
+*/
+
+/**
+ * Classe da CatCard
+ */
 class CatCard {
+    /**
+     * @class CatCard construtor
+     * @param {String} name nome do card
+     * @param {Status} status status do card
+     * @param {String} description descricao do card
+     * @param {Array<String>} tags array de ate 2 tags
+     * @param {int} id id do card no banco de dados
+     * @param {String} uuid uuid do card no banco de dado
+     */
     constructor(name, status, description, tags, id, uuid) {
         this._name = name
         this._status =  status
@@ -8,58 +27,99 @@ class CatCard {
         this.__uuid = uuid
     }
 
+    /**
+     * Setter de name
+     */
     set name(name) {
         this._name = name
     }
 
+    /**
+     * Getter de name
+     */
     get name() {
         return this._name
     }
 
+    /**
+     * Adiciona tag para array
+     * @param {String} tag 
+     */
     pushTag(tag) {
         if (!this._tags.includes(tag))
             this._tags.push(tag)
     }
 
+    /**
+     * Getter do array de tags
+     */
     get tags() {
         return this._tags
     }
 
+    /**
+     * Removedor de uma tag
+     * @param {String} tag 
+     */
     removeTag(tag) {
         if (this._tags.includes(tag))
             this._tags.splice(this._tags.indexOf(tag), 1)
     } 
 
+    /**
+     * Setter de description
+     */
     set description(description) {
         this._description = description
     }
 
+    /**
+     * Getter de description
+     */
     get description() {
         return this._description
     }
 
+    /**
+     * Setter de status
+     */
     set status(status) {
         this._status = status
     }
 
+    /**
+     * Getter de status
+     */
     get status() {
         return this._status
     }
 
+    /**
+     * Setter de id
+     */
+    set id(id) {
+        this.__id = id
+    }
+ 
+    /**
+     * Getter de id
+     */
     get id() {
         return this.__id
     }
 
-    set id(id) {
-        this.__id = id
-    }
-
-    get uuid() {
-        return this.__uuid
-    }
-
+    /**
+     * Setter de uuid
+     */
     set uuid(uuid) {
         this.__uuid = uuid
+    }
+
+    /**
+     * Getter de uuid
+     */
+    get uuid() {
+        return this.__uuid
     }
 }
 
