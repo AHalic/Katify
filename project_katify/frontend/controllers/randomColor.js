5a5cb47760a6d74436504dd93f22db974f019fa8
AHalic <sophie.gama@aluno.ufes.br>
2021-10-08 11:59:50 -0300

    :bulb: docs: adds constant description

--- project_katify/frontend/controllers/randomColor.js
+++ project_katify/frontend/controllers/randomColor.js
@@ -1,3 +1,18 @@
+/*
+ * Descricao: Arquivo que gera uma cor aleatória usada para as tags
+ * Data: Outubro 2021
+ * @version: 1.0
+ * @author Beatriz Maia & Sophie Dilhon
+ */ 
+
+
+/**
+ * Objeto com as possíveis cores
+ * - pink
+ * - purple
+ * - teal
+ * - blue
+ */
 const Color = {
     0: "color-pink",
     1: "color-purple",
@@ -5,6 +20,10 @@ const Color = {
     3: "color-blue"
 }
 
+/**
+ * Gera um valor aleatório e escolhe uma cor
+ * @returns {string} cor gerada
+ */
 function randomColor() {
     let index = Math.floor(Math.random() * 4)
     return Color[index]
