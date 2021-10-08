61818dd787fdb8fd2cfff1c47c322e5522a4a7d9
AHalic <sophie.gama@aluno.ufes.br>
2021-10-08 05:00:13 -0300

    :bulb: docs: adds comments to class

--- project_katify/backend/infra/tables.js
+++ project_katify/backend/infra/tables.js
@@ -1,4 +1,12 @@
+/**
+ * Classe que representa as tabelas do banco de dados
+ */
 class Tables {
+    /**
+     * Inicializa a classe criando as tabelas
+     * @param {*} conexao - constante que guarda 
+     * as informações da conexão com o sql
+     */
     init(conexao) {
         this.conexao = conexao
 
@@ -7,6 +15,13 @@ class Tables {
         this.createBoardTable()
     }
 
+    /**
+     * Método responsável por criar a tabela que guarda os dados 
+     * de todos os cards do usuário. 
+     * 
+     * A tabela contém 7 colunas:
+     *  uuid, name, tag1, tag2, description, status e id.
+     */
     createCardTable() {
         const sql = 'CREATE TABLE IF NOT EXISTS Cards (id int NOT NULL AUTO_INCREMENT, uuid varchar(40) NOT NULL, name varchar(45) NOT NULL, tag1 varchar(15), tag2 varchar(15), description text, status varchar(15) NOT NULL, PRIMARY KEY(id))'
 
@@ -19,6 +34,13 @@ class Tables {
         })
     }
 
+    /**
+     * Método responsável por criar a tabela que guarda os 
+     * dados de todos os workspaces do usuário. 
+     * 
+     * A tabela contém 3 colunas:
+     *  uuid, name e id.
+     */
     createBoardTable() {
         const sql = 'CREATE TABLE IF NOT EXISTS Boards (id int NOT NULL AUTO_INCREMENT, uuid varchar(40) NOT NULL, name varchar(45) NOT NULL, PRIMARY KEY(id))';
 
