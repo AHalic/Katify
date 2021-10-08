9f12795211bce71eb971a1102a244f68ac9fb922
AHalic <sophie.gama@aluno.ufes.br>
2021-10-08 11:18:56 -0300

    :bulb: docs: changes file documentation

--- project_katify/frontend/controllers/boardPost.js
+++ project_katify/frontend/controllers/boardPost.js
@@ -1,4 +1,4 @@
-/**
+/*
  * Descricao: Funcoes relacionadas a adicao de um board
  * Data: Outubro 2021
  * @version: 1.0
@@ -7,20 +7,21 @@
 
 import api from '../api.js'
 
-// Elementos referente a adicao de um board na pagina
+// Elementos referente ao form de adicao de um board na pagina
 let workspaceInput = document.getElementsByClassName("form-add-board")[0]
 let workspaceForm = document.getElementById("addBoard-form")
 
 /**
- * Evento em submit para alterar o nome do workspace
+ * Evento em submit para criar um board novo conforme os dados fornecidos
  */
 workspaceForm.addEventListener("submit", (event) => {
 	event.preventDefault()
 	if (!workspaceInput.value) {
+		// Caso o Input esteja vazio, não será criado o board
 		workspaceInput.innerHTML = "Não preenchido"
 	}
 	else {
-		// Cria board
+		// Cria board e o adiciona ao bd
 		api.post(`/`, { name: `${workspaceInput.value}` })
 			.then(res => {
 				document.getElementsByClassName("closeBoardModal")[0].click()
