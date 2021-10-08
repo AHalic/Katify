a4bad629ed08c8baca77eb43fc2eec6ac0d843b1
AHalic <sophie.gama@aluno.ufes.br>
2021-10-08 12:00:15 -0300

    :bulb: docs: adds delete description

--- project_katify/frontend/controllers/cardsDelete.js
+++ project_katify/frontend/controllers/cardsDelete.js
@@ -1,6 +1,17 @@
+/*
+ * Descricao: Funcoes relacionadas a remocao de um card
+ * Data: Outubro 2021
+ * @version: 1.0
+ * @author Beatriz Maia & Sophie Dilhon
+ */ 
+
 import api from '../api.js'
 const boarduuid = document.URL.split('/')[3]
 
+/**
+ * Função que remove o card após confirmação do alert
+ * @param {int} id identificador do card
+ */
 function deleteCard(id) {
     swal({
         text: "Are you sure you want to delete this board?",
@@ -8,9 +19,9 @@ function deleteCard(id) {
         className: "alert-content",
     }).then((value) => {
             if (value) {
+                // remove do banco de dados e do html
                 api.delete(`/${boarduuid}/${id}`)
                     .then(res => {
-                        // window.location = url
                         document.getElementsByClassName("close-edit")[0].click()
                         document.getElementById(id).remove();
                     })
