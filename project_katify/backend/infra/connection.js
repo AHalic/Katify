94a05adde00a5a5390a56fc32708b56efefc0e56
AHalic <sophie.gama@aluno.ufes.br>
2021-10-08 05:00:29 -0300

    :bulb: docs: adds comments to const

--- project_katify/backend/infra/connection.js
+++ project_katify/backend/infra/connection.js
@@ -1,11 +1,14 @@
 const mysql = require('mysql2')
 
+/**
+ * Configurações da conexão com mysql
+ */
 const conexao = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'pass', // Your password
-    database: 'katify'
+    password: 'pass', // Sua senha mysql
+    database: 'katify' // Nome do db criado
 })
 
 module.exports = conexao
